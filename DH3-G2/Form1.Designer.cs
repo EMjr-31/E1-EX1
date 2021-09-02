d060bbd79a941ba844bc473c861d8966c40bacc8
Emjr31 <adonay.chacon12@gmail.com>
2021-09-02 11:30:45 -0600

    Disenio de interfaz y primer funcionamiento

---
+++ DH3-G2/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace DH3_G2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Solucion = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txta = new System.Windows.Forms.TextBox();
+            this.txtb = new System.Windows.Forms.TextBox();
+            this.txtc = new System.Windows.Forms.TextBox();
+            this.txtX1 = new System.Windows.Forms.TextBox();
+            this.txtX2 = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.Solucion.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(140, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Formula Cuadratica";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtc);
+            this.groupBox1.Controls.Add(this.txtb);
+            this.groupBox1.Controls.Add(this.txta);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(53, 75);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(361, 52);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos";
+            // 
+            // Solucion
+            // 
+            this.Solucion.Controls.Add(this.btnCalcular);
+            this.Solucion.Controls.Add(this.txtX2);
+            this.Solucion.Controls.Add(this.txtX1);
+            this.Solucion.Controls.Add(this.label7);
+            this.Solucion.Controls.Add(this.label5);
+            this.Solucion.Location = new System.Drawing.Point(53, 143);
+            this.Solucion.Name = "Solucion";
+            this.Solucion.Size = new System.Drawing.Size(361, 71);
+            this.Solucion.TabIndex = 2;
+            this.Solucion.TabStop = false;
+            this.Solucion.Text = "Solucion";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "a:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(134, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "b:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(241, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "c:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(134, 31);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "X1:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(234, 31);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "X2:";
+            // 
+            // txta
+            // 
+            this.txta.Location = new System.Drawing.Point(41, 17);
+            this.txta.Name = "txta";
+            this.txta.Size = new System.Drawing.Size(53, 20);
+            this.txta.TabIndex = 3;
+            // 
+            // txtb
+            // 
+            this.txtb.Location = new System.Drawing.Point(156, 17);
+            this.txtb.Name = "txtb";
+            this.txtb.Size = new System.Drawing.Size(53, 20);
+            this.txtb.TabIndex = 4;
+            // 
+            // txtc
+            // 
+            this.txtc.Location = new System.Drawing.Point(264, 17);
+            this.txtc.Name = "txtc";
+            this.txtc.Size = new System.Drawing.Size(53, 20);
+            this.txtc.TabIndex = 5;
+            // 
+            // txtX1
+            // 
+            this.txtX1.Location = new System.Drawing.Point(156, 28);
+            this.txtX1.Name = "txtX1";
+            this.txtX1.Size = new System.Drawing.Size(53, 20);
+            this.txtX1.TabIndex = 6;
+            // 
+            // txtX2
+            // 
+            this.txtX2.Location = new System.Drawing.Point(264, 28);
+            this.txtX2.Name = "txtX2";
+            this.txtX2.Size = new System.Drawing.Size(53, 20);
+            this.txtX2.TabIndex = 7;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(31, 31);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 8;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 242);
+            this.Controls.Add(this.Solucion);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.Solucion.ResumeLayout(false);
+            this.Solucion.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtc;
+        private System.Windows.Forms.TextBox txtb;
+        private System.Windows.Forms.TextBox txta;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox Solucion;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtX2;
+        private System.Windows.Forms.TextBox txtX1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ DH3-G2/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DH3_G2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            ////Variables 
+            double a = Convert.ToDouble(txta.Text);
+            double b = Convert.ToDouble(txtb.Text);
+            double c = Convert.ToDouble(txtc.Text);
+            double x1, x2;
+            ///Proceso de calculo de X1 y X2
+            try
+            {
+                x1 = (-b + Math.Sqrt((Math.Pow(b, 2) - (4 * a * c)))) / (2 * a);
+                x2 = (-b - Math.Sqrt((Math.Pow(b, 2) - (4 * a * c)))) / (2 * a);
+                ///MOstrar resultados
+                txtX1.Text = x1.ToString();
+                txtX2.Text = x2.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Revise que los datos ingresados esten correctos.. ."+ex);
+            }
+
+        }
+    }
+}
